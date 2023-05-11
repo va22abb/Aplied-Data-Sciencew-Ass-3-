8c443876ba7efc4993f4712fded78c18a1802762
va22abb <125890605+va22abb@users.noreply.github.com>
2023-05-11 17:31:50 +0100

    Add files via upload

---
+++ 22029348.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 6 15:34:18 2023
+
+@author: Victoria
+"""
+
+# import libraries
+import warnings
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.cluster import KMeans
+from scipy.optimize import curve_fit as cf
+
+
+# load data into pandas data frame
+def read_data(fname):
+    data = pd.read_csv(fname)
+    df = pd.DataFrame(data)
+    dataT = pd.read_csv(fname, header=None, index_col=0).T
+    dfT = pd.DataFrame(dataT)
+    dfT = dfT.rename(columns={"Country Name": "Year"})
+    return df, dfT
+
+
+# The function convert def to numbers
+def convert_to_numbers(df, column_list):
+    df[column_list] = df[column_list].apply(pd.to_numeric, errors='coerce')
+    df = df.fillna(0)
+    return df
+
+
+def objective(x, a, b, c):
+    return (a * x) + (b * x ** 2) + c
+
+
+def curve_fit_plot(dft, df1t, country):
+    country_new = "New " + country
+    dft = convert_to_numbers(dft, [country])
+    df1t = convert_to_numbers(df1t, [country])
+    popt, _ = cf(objective, dft[country], df1t[country])
+    df1t[country_new] = objective(dft[country], *popt)
+    plt.plot(dft[country], df1t[country], color='red')
+    plt.plot(dft[country], df1t[country_new], color='blue')
+    cr = np.std(df1t[country]) / np.sqrt(len(df1t[country]))
+    plt.fill_between(dft[country], (df1t[country] - cr), (df1t[country] + cr),
+                     color='b', alpha=0.1)
+    plt.xlabel(dft.columns[0])
+    plt.ylabel(df1t.columns[0])
+    plt.legend(["Expected", "Predicted"], loc="upper right")
+    plt.title("Curve Fit of " + country)
+
+
